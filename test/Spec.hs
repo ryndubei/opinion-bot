e343971f8b5e9a828c338575c4cf027aa1c64b8e
ryndubei <114586905+ryndubei@users.noreply.github.com>
2023-03-07 00:14:05 +0000

    Test for DataChannel

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,13 @@
+import Lib
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Lib" $
+    describe "DataChannel" $ do
+      it "can manage state" $ do
+        dataChannel <- spawnDataManager (+) 0
+        send 1 dataChannel
+        send 1 dataChannel
+        reply1 <- request dataChannel
+        reply1 `shouldBe` 2
