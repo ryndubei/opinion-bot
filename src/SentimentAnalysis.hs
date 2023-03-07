fc665dcc9ecb37c7439d572b253a2a91e907a9b1
ryndubei <114586905+ryndubei@users.noreply.github.com>
2023-03-07 11:25:36 +0000

    Revise sentiment analysis

--- src/SentimentAnalysis.hs
+++ src/SentimentAnalysis.hs
@@ -3,7 +3,7 @@ module SentimentAnalysis ( analyseRawMessages, wordInvariant ) where
 import Data.Text (Text)
 import qualified Data.Text as T
 
-import Data.Char (isLetter, isLower)
+import Data.Char (isLetter)
 
 import SentimentAnalysis.SentimentData 
   ( SentimentData
@@ -28,7 +28,6 @@ type Sentence = [Text]
 -- | Turn a Text into a list of words containing only lowercase letters,
 -- split by non-letter characters.
 toSentence :: Text -> Sentence
--- TODO: make it split a text into actual sentences
 toSentence = T.split (not . isLetter) . T.toLower
 
 -- | Return the overall sentiment of sentences containing a particular word
@@ -36,7 +35,11 @@ analyseWordSentiments :: SentimentData -> Text -> [Sentence] -> Double
 analyseWordSentiments sentimentMap word sentences =
   -- We do not want to include the target word itself in calculations, as it
   -- might be seen as positive or negative by itself
-  let sentences' = map (filter (/=word)) . filter (word `elem`) $ sentences
+  let sentences' = 
+        filter (not . null)
+        . map (filter (/=word)) 
+        . filter (word `elem`) 
+        $ sentences
    in analyseSentiments sentimentMap sentences'
 
 -- | Return the overall positivity/negativity of a list of sentences
@@ -49,11 +52,12 @@ analyseSentiments sentimentMap sentences =
 -- | Return the positivity/negativity of a single sentence 
 -- as a value between -1 and 1.
 analyseSentiment :: SentimentData -> Sentence -> Double
-analyseSentiment sentimentMap sentence = case sentence of
-  [] -> 0
-  _  -> let values = map (`lookupSentiment` sentimentMap) sentence
-         in average values
+analyseSentiment sentimentMap sentence =
+  let values = filter (/=0) $ 
+        map (`lookupSentiment` sentimentMap) sentence
+   in average values
 
-average :: Fractional a => [a] -> a
-average [] = 0
-average a = sum a / (realToFrac . length) a
+average :: (Foldable t, Fractional a) => t a -> a
+average xs 
+  | null xs = 0
+  | otherwise = sum xs / (realToFrac . length) xs
